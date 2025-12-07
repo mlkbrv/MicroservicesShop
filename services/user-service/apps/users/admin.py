89ced365a8161438019ef4d43332683fecb24369
E <emilakbarov857@gmail.com>
2025-12-07 21:56:54 +0400

    feat: added users app functionality

--- services/user-service/apps/users/admin.py
+++ services/user-service/apps/users/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User, UserProfile
 
-# Register your models here.
+class UserProfileInline(admin.StackedInline):
+    model = UserProfile
+    can_delete = False
+    verbose_name_plural = 'Profile'
+
+class CustomUserAdmin(UserAdmin):
+    inlines = (UserProfileInline,)
+    list_display = ('email', 'username', 'first_name', 'last_name', 'is_staff')
+    ordering = ('email',)
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'username')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+
+admin.site.register(User, CustomUserAdmin)
--- services/user-service/apps/users/views.py
+++ services/user-service/apps/users/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.responce import Responce
+from rest_framework.permissions import IsAuthenticated
+from .models import User, UserProfile
+from .serializers import UserWithProfileSerializer,UserProfileSerializer,UserRequestSerializer
 
-# Create your views here.
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserRequestSerializer
+    permission_classes = []
+
+class ProfileView(generics.RetrieveUpdateAPIView):
+    queryset = UserProfile.objects.all()
+    serializer_class = UserProfileSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
+
+class ProfileUpdateView(generics.UpdateAPIView):
+    serializer_class = UserProfileSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        profile, created = UserProfile.objects.get_or_create(user=self.request.user)
+        return profile
